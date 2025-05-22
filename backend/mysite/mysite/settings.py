c1aa61230dfbc61f6ea056dbc32aeb5f5514d8a3
Lars de Wit <larsdewit@outlook.com>
2025-05-22 19:52:39 +0200

    fix: add localhost to ALLOWED_HOSTS in Django settings and update psycopg2-binary version in requirements

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1", "motivatie-tool.larsdewit.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "motivatie-tool.larsdewit.com"]
 
 
 # Application definition
