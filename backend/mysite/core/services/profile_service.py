e9d5881b889e226ffd51e8dc03e287bab7197311
Lars de Wit <larsdewit@outlook.com>
2025-05-23 12:09:11 +0200

    fix: enhance logging in PhaseService and ProfileService for better traceability and error handling

--- backend/mysite/core/services/profile_service.py
+++ backend/mysite/core/services/profile_service.py
@@ -1,15 +1,19 @@
 from ..repositories.profile_repository import *
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ProfileService:
     def __init__(self):
         self.profile_repository = ProfileRepository()
 
     def get_profile_by_user_id(self, id: int):
+        logger.info(f"Fetching profile for user_id={id}")
         profile = self.profile_repository.get_profile_by_user_id(id)
     
         if not profile:
+            logger.warning(f"No profile found for user_id={id}")
             raise Profile.DoesNotExist
         
-        print(profile)
-        
+        logger.debug(f"Profile found for user_id={id}: {profile}")
         return profile
