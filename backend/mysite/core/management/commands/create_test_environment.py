3c84964ec33d695894eaf5d8e9f2ab35a2aed269
Lars de Wit <larsdewit@outlook.com>
2025-05-23 18:59:18 +0200

    feat: add custom management command to print a greeting

---
+++ backend/mysite/core/management/commands/create_test_environment.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = 'Prints Hello'
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Hello from a custom command!'))
